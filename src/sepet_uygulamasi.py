bb3c9a248f6947ec80601e436f85890938045f3d
Semih Gunes <f241229091@ktun.edu.tr.com>
2026-05-12 17:24:03 +0300

    faz1: fabrika deseni tamamlandi

--- src/sepet_uygulamasi.py
+++ src/sepet_uygulamasi.py
@@ -1,28 +1,57 @@
-class Urun:
-    def __init__(self,ad,fiyat):
+from abc import ABC, abstractmethod
+class indirimstratejisi(ABC):
+    @abstractmethod
+    def hesapla(self,fiyat):
+        pass
+class yilbasiindirimi(indirimstratejisi):
+    def hesapla(self, fiyat):
+        return fiyat*0.8
+class ogrenciindirimi(indirimstratejisi):
+    def hesapla(self, fiyat):
+        return fiyat*0.7
+class kuponindirimi(indirimstratejisi):
+    def hesapla(self, fiyat):
+        return fiyat*0.9
+class urun:
+    def __init__(self, fiyat,ad):
         self.ad=ad
         self.fiyat=fiyat
-class sepet:
-    def __init__(self,):
-        self.liste=[]
-    def urun_ekle(self,urun):
-        self.liste.append(urun)
-
-    def hesaplama(self,indirim_tipi):
-        toplam=sum(u.fiyat for u in self.liste)
-        if indirim_tipi=="YILBASI":
-            return toplam*0.8
-        elif indirim_tipi=="OGRENCI":
-            return toplam*0.7
-        elif indirim_tipi=="KUPON10":
-            return toplam*0.9
+    def __str__(self):
+        return f"{self.ad}: {self.fiyat} TL"    
+class indirimfab:
+    @staticmethod
+    def indirimolustur(tip):
+        if tip=="yilbasi":
+            return yilbasiindirimi()
+        elif tip=="ogrenci":
+            return ogrenciindirimi()
+        elif tip=="kupon":
+            return kuponindirimi()
         else:
-            return toplam   
-        
-sepet1=sepet()
-urun1=Urun("kitap",100)
-urun2=Urun("Klavye",250)
-sepet1.urun_ekle(urun1)
-sepet1.urun_ekle(urun2)
-tutar=sepet1.hesaplama("OGRENCI")
-print(f"Odenecek toplam tutar: {tutar}")
+            raise ValueError("Gecersiz indirim tipi")
+class urunlab:
+    @staticmethod
+    def urunolustur(ad,fiyat):
+        return urun(fiyat,ad)
+class sepet:
+    def __init__(self):
+        self.liste=[]
+
+    def urunekle(self,urun):
+        self.liste.append(urun) 
+    def hesapla(self,indirimtipi):
+        toplam=sum(urun.fiyat for urun in self.liste)
+        strateji= indirimfab.indirimolustur(indirimtipi)
+        if strateji:
+            return strateji.hesapla(toplam)     
+        return toplam
+    
+if __name__=="__main__":   
+    sepetim=sepet()
+    sepetim.urunekle(urunlab.urunolustur("Kitap",100))
+    sepetim.urunekle(urunlab.urunolustur("Kalem",20))
+    sepetim.urunekle(urunlab.urunolustur("Defter",50))
+    print("Toplam fiyat:",sepetim.hesapla("yilbasi"))
+    print("Toplam fiyat:",sepetim.hesapla("ogrenci"))
+    print("Toplam fiyat:",sepetim.hesapla("kupon"))
+    
