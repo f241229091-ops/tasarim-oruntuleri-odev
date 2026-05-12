1f0e411f73bdaa12fa8a56aa2a28be649740bf15
Semih Gunes <f241229091@ktun.edu.tr.com>
2026-05-12 17:36:51 +0300

    faz2: decorator ile indirim birlestirme tamamlandi

--- src/sepet_uygulamasi.py
+++ src/sepet_uygulamasi.py
@@ -1,57 +1,76 @@
 from abc import ABC, abstractmethod
+
 class indirimstratejisi(ABC):
     @abstractmethod
-    def hesapla(self,fiyat):
+    def hesapla(self, fiyat):
         pass
+
 class yilbasiindirimi(indirimstratejisi):
     def hesapla(self, fiyat):
-        return fiyat*0.8
+        return fiyat * 0.8
+
 class ogrenciindirimi(indirimstratejisi):
     def hesapla(self, fiyat):
-        return fiyat*0.7
+        return fiyat * 0.7
+
 class kuponindirimi(indirimstratejisi):
     def hesapla(self, fiyat):
-        return fiyat*0.9
+        return fiyat * 0.9
+
+class EkIndirimDecorator(indirimstratejisi):
+    def __init__(self, temel_indirim, ek_oran):
+        self.temel_indirim = temel_indirim
+        self.ek_oran = ek_oran
+
+    def hesapla(self, fiyat):
+        ara_sonuc = self.temel_indirim.hesapla(fiyat)
+        return ara_sonuc * self.ek_oran
+
 class urun:
-    def __init__(self, fiyat,ad):
-        self.ad=ad
-        self.fiyat=fiyat
-    def __str__(self):
-        return f"{self.ad}: {self.fiyat} TL"    
+    def __init__(self, fiyat, ad):
+        self.ad = ad
+        self.fiyat = fiyat
+    def __repr__(self):
+        return f"{self.ad}: {self.fiyat} TL"
+
 class indirimfab:
     @staticmethod
     def indirimolustur(tip):
-        if tip=="yilbasi":
-            return yilbasiindirimi()
-        elif tip=="ogrenci":
-            return ogrenciindirimi()
-        elif tip=="kupon":
-            return kuponindirimi()
-        else:
-            raise ValueError("Gecersiz indirim tipi")
+        if tip == "yilbasi": return yilbasiindirimi()
+        elif tip == "ogrenci": return ogrenciindirimi()
+        elif tip == "kupon": return kuponindirimi()
+        return None
+
 class urunlab:
     @staticmethod
-    def urunolustur(ad,fiyat):
-        return urun(fiyat,ad)
+    def urunolustur(ad, fiyat):
+        return urun(fiyat, ad)
+
 class sepet:
     def __init__(self):
-        self.liste=[]
+        self.liste = []
 
-    def urunekle(self,urun):
-        self.liste.append(urun) 
-    def hesapla(self,indirimtipi):
-        toplam=sum(urun.fiyat for urun in self.liste)
-        strateji= indirimfab.indirimolustur(indirimtipi)
+    def urunekle(self, urun_nesnesi):
+        self.liste.append(urun_nesnesi)
+
+    def hesapla(self, strateji_veya_tip):
+        toplam = sum(u.fiyat for u in self.liste)
+        
+        if isinstance(strateji_veya_tip, indirimstratejisi):
+            return strateji_veya_tip.hesapla(toplam)
+        
+        strateji = indirimfab.indirimolustur(strateji_veya_tip)
         if strateji:
-            return strateji.hesapla(toplam)     
+            return strateji.hesapla(toplam)
+        
         return toplam
+
+if __name__ == "__main__":
+    sepetim = sepet()
+    sepetim.urunekle(urunlab.urunolustur("Telefon", 1000))
+    sepetim.urunekle(urunlab.urunolustur("Kılıf", 100))
+
+    temel = ogrenciindirimi()
+    kombo = EkIndirimDecorator(temel, 0.9)
     
-if __name__=="__main__":   
-    sepetim=sepet()
-    sepetim.urunekle(urunlab.urunolustur("Kitap",100))
-    sepetim.urunekle(urunlab.urunolustur("Kalem",20))
-    sepetim.urunekle(urunlab.urunolustur("Defter",50))
-    print("Toplam fiyat:",sepetim.hesapla("yilbasi"))
-    print("Toplam fiyat:",sepetim.hesapla("ogrenci"))
-    print("Toplam fiyat:",sepetim.hesapla("kupon"))
-    
+    print(f"Toplam: {sepetim.hesapla(kombo)} TL")
